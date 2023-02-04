b2a48922b4c4755c171ee4f246c34ec585324cc4
Narmadhadevi <37329367+Narmadhadevi@users.noreply.github.com>
2023-02-04 20:03:07 +0530

    Delete Student.java

--- Student.java
+++
@@ -1,9 +0,0 @@
-package week1.assignment1;
-
-public class Student {
-	String studentName="Narmadha";
-	int rollNo=921;
-	String collegeName="SRM institute";
-	int markScored=189;
-	float cgpa=8.8f;
-}
