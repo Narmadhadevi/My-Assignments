6a48066e9ba1e3edf97b26ae89f999889873d763
Narmadhadevi <37329367+Narmadhadevi@users.noreply.github.com>
2023-02-07 16:30:36 +0530

    Week1
    Week1

---
+++ assignment1/Car.java
@@ -0,0 +1,18 @@
+package week1.assignment1;
+
+public class Car {
+	public void applyBreak() {
+		System.out.println("Applied break");
+		
+	}
+public void soundHorn() {
+	System.out.println("Sound horn");
+	
+}
+public static void main(String[] args) {
+	Car door=new Car();
+	door.applyBreak();
+	door.soundHorn();
+	
+}
+}
---
+++ assignment1/Student.java
@@ -0,0 +1,9 @@
+package week1.assignment1;
+
+public class Student {
+	String studentName="Narmadha";
+	int rollNo=921;
+	String collegeName="SRM institute";
+	int markScored=189;
+	float cgpa=8.8f;
+}
