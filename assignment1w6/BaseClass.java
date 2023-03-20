03f54229f24199b75b1d47fbdf3be5f5bbdf48e8
Narmadhadevi <37329367+Narmadhadevi@users.noreply.github.com>
2023-03-20 21:44:49 +0530

    Week 6

---
+++ assignment1w6/BaseClass.java
@@ -0,0 +1,40 @@
+package week6.assignment1w6;
+
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public ChromeDriver driver;
+	
+	
+	@BeforeMethod
+public void pre() {
+	WebDriverManager.chromiumdriver().setup();
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("--remote-allow-origins=*");
+	driver = new ChromeDriver(options);
+	driver.manage().window().maximize();
+	driver.get("http://leaftaps.com/opentaps/");
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.findElement(By.id("username")).sendKeys("demosalesManager");
+	driver.findElement(By.id("password")).sendKeys("crmsfa");
+	driver.findElement(By.className("decorativeSubmit")).click();
+	driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("Leads")).click();
+	
+}
+	@AfterMethod
+	public void post() {
+	driver.close();
+	}
+}
